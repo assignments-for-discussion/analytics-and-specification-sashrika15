52087857fc26f0f7fb34f0445d64315df92a7027
sashrika15 <sashrika.surya@gmail.com>
2021-12-10 12:34:13 +0530

    fix: Passed test for ignoring NaN in input

--- average.js
+++ average.js
@@ -1,6 +1,10 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  // Filtering array to remove NaN
+  const NanRemoved = numbers.filter((num) => !Number.isNaN(num));
+
+  // Computing average from updated array
+  return NanRemoved.reduce((p, c)=> p + c, 0) / NanRemoved.length;
 }
 
 module.exports = {average};
