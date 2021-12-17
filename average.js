f30d7c209dc2e09f319f51547246f8b5c3986b6b
sashrika15 <sashrika.surya@gmail.com>
2021-12-17 15:20:38 +0530

    style: Converted to camelCase

--- average.js
+++ average.js
@@ -1,15 +1,15 @@
 
 function average(numbers) {
   // Filtering array to remove NaN
-  const NanRemoved = numbers.filter((num) => !Number.isNaN(num));
+  const nanRemoved = numbers.filter((num) => !Number.isNaN(num));
 
   // Calculating number of NaNs and returning NaN if percentage of NaNs is >50
-  const NoOfNaNs = numbers.length -NanRemoved.length;
-  if (NoOfNaNs/numbers.length>0.5) {
+  const noOfNaNs = numbers.length - nanRemoved.length;
+  if (noOfNaNs/numbers.length>0.5) {
     return NaN;
   } else {
   // Computing average from updated array
-    return NanRemoved.reduce((p, c)=> p + c, 0) / NanRemoved.length;
+    return nanRemoved.reduce((p, c)=> p + c, 0) / nanRemoved.length;
   }
 }
 
